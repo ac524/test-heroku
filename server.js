77cb33312674069313cdd19c63ad4d3944faf9c1
Anthony Brown <ac524.brown@gmail.com>
2020-12-12 13:32:04 -0800

    Added a new test route.

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 var PORT = process.env.PORT || 3001;
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World');
 })
+
+app.get('/test', function (req, res) {
+    res.send('Hello Test World');
+});
  
 app.listen( PORT );
